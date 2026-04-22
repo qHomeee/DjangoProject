f00f537c91d98a749cbbfca13db72e6f8dbed133
DmitriyZaulin <163876691+qHomeee@users.noreply.github.com>
2026-04-22 18:47:07 +0300

    Update settings.py

--- sneaker_store/settings.py
+++ sneaker_store/settings.py
@@ -6,6 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "dev-sneaker-store-key")
 DEBUG = os.environ.get("DEBUG", "1") == "1"
 ALLOWED_HOSTS = os.environ.get(
+    "djangoproject-production-0188.up.railway.app",
     "ALLOWED_HOSTS",
     "localhost,127.0.0.1,0.0.0.0",
 ).split(",")
