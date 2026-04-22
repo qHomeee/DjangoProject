d7b7b811f819dcd81b0214fd8d37bae2c833795f
DmitriyZaulin <wanttodie37@gmail.com>
2026-04-22 11:53:40 +0300

    Create sneakers application

---
+++ sneakers/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class SneakersConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "sneakers"
+    verbose_name = "Магазин кроссовок"
